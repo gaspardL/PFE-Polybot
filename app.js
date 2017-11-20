0ba717805131f5f73d77c3d6070f68ec99f349b7
Gaspard Lacroix <gaspard.lacroix@etu.unice.fr>
2017-11-20 01:35:15 +0100

    Premier slackbot (mis au gout du jour)

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var hellobot = require('./logic/hellobot');
+var RtmClient = require('@slack/client').RtmClient;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var bot_token = process.env.SLACK_BOT_TOKEN || '';
 
-var app = express();
-var port = process.env.PORT || 3000;
+var rtm = new RtmClient(bot_token);
+rtm.start();
 
-// body parser middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// test route
-app.get('/', function (req, res) { res.status(200).send('Hello world!'); });
-
-app.post('/hello', hellobot);
-
-// error handler
-app.use(function (err, req, res, next) {
-	console.error(err.stack);
-	res.status(400).send(err.message);
-});
-
-app.listen(port, function () {
-	console.log('Slack bot listening on port ' + port);
+rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+  if (message.text === "Hello.") {
+    var channel = "D82METR8T"; //could also be a channel, group, DM, or user ID (C1234), or a username (@don)
+    rtm.sendMessage("Hello <@" + message.user + ">!", message.channel);
+  }
 });
