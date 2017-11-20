6eaea4a35bff6a9775056cab9ca42eb90745e6cf
Gaspard Lacroix <gaspard.lacroix@etu.unice.fr>
2017-11-20 09:22:36 +0100

    Charge quand message reçu

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var bot_token = process.env.SLACK_BOT_TOKEN || '';
 var rtm = new RtmClient(bot_token);
 rtm.start();
 
-for(var i = 0; i < 1000; i++){
-	rtm.sendMessage("hi", "C82QE5S66");
-}
+rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+	if (message.text === "charge") {
+		var channel = "D82METR8T"; //could also be a channel, group, DM, or user ID (C1234), or a username (@don)
+		for(var i =0; i < 1000; i++){
+			rtm.sendMessage("hi", message.channel);
+		}
+	}
+});
