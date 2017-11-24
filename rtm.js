9e3c351ec1fbc25222a817e90656beb0ede49e73
Robin Alonzo <alonzo.robin7@gmail.com>
2017-11-24 14:57:10 +0100

    Detecte les messages d'erreur

--- rtm.js
+++ rtm.js
@@ -15,16 +15,33 @@ console.log("Server connected to slackbot ("+bot_token+")");
 
 var messages = [];
 
+var message_sending_timeout = null;
+
+function start_sending_messages(){
+    if(!message_sending_timeout)
+        message_sending_timeout = setInterval(send_message,100);
+}
+
+start_sending_messages();
+
 function send_message(){
 	if(messages.length >= 1){
-		let request = messages[0];
+		let request = messages.shift();
 		console.log("Send: "+request.message);
-        rtm.sendMessage(request.message, request.channel);
-        messages.shift();
+        rtm.sendMessage(request.message, request.channel,function(error,m){
+            if(error){
+                console.log("Error:");
+                console.log(error);
+                clearInterval(message_sending_timeout);
+                message_sending_timeout = null;
+                messages.unshift(request);
+                setTimeout(start_sending_messages,1000)
+            }
+		});
 	}
 }
 
-setInterval(send_message,100);
+
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
     // console.log("<=== "+message.user+" : "+ message.text);
