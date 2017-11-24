18f39ca662c50eb55394201789bcd3f8d1b2b413
Robin Alonzo <alonzo.robin7@gmail.com>
2017-11-24 10:25:34 +0100

    Ajout de fille d'attente de messages a envoyer #1

--- rtm.js
+++ rtm.js
@@ -13,11 +13,24 @@ var rtm = new RtmClient(bot_token);
 rtm.start();
 console.log("Server connected to slackbot ("+bot_token+")");
 
+var messages = [];
+
+function send_message(){
+	if(messages.length >= 1){
+		let request = messages[0];
+        rtm.sendMessage(request.message, request.channel);
+		messages.shift();
+	}
+}
+
+setInterval(send_message,100);
+
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
     // console.log("<=== "+message.user+" : "+ message.text);
 	let result = dispatcher.dispatch(message.text);
 	if(result){
-		rtm.sendMessage(result, message.channel);
+		messages.push({message:result,channel:message.channel});
+		// rtm.sendMessage(result, message.channel);
         // console.log("===> ("+message.user+") : "+ result);
 	}
 });
